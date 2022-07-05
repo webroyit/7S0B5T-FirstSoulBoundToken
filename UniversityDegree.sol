dac8e33d1f1fd7d5dc6279522fcd5413d3eb5d80
royweb <xytank@gmail.com>
2022-07-05 12:53:07 -0400

    Import Openzeppelin Counters and ERC721URIStorage contracts

--- UniversityDegree.sol
+++ UniversityDegree.sol
@@ -1,6 +1,14 @@
- // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-contract UniversityDegree {
-    
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract UniversityDegree is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    // This function get call once when you deploy the contract
+    constructor() ERC721("UniversityDegree", "Degree") {
+
+    }
 }
