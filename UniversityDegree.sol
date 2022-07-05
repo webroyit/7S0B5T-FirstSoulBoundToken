22675e46338e639c0bc142fd7f5d482490e89f74
royweb <xytank@gmail.com>
2022-07-05 13:29:50 -0400

    Add a function to issue degree

--- UniversityDegree.sol
+++ UniversityDegree.sol
@@ -5,10 +5,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract UniversityDegree is ERC721URIStorage {
+
     using Counters for Counters.Counter;
 
     // This function get call once when you deploy the contract
     constructor() ERC721("UniversityDegree", "Degree") {
 
+    }
+
+    // Record which addresses have a degree issued
+    mapping(address => bool) public issueDegrees;
+
+    function issueDegree(address to) external {
+        issueDegrees[to] = true;
     }
 }
