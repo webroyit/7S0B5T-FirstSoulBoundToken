5073b5211526fbe8dd2dfd4033814e414ee2e92f
royweb <xytank@gmail.com>
2022-07-05 14:07:30 -0400

    Add a modifier to check for owner

---
+++ README.md
@@ -0,0 +1,4 @@
+# Soul Bound Token
+
+## Notes
+- `modifier` is a piece of code that you call before the next function code gets executed
--- UniversityDegree.sol
+++ UniversityDegree.sol
@@ -6,17 +6,24 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract UniversityDegree is ERC721URIStorage {
 
+    address owner;
+
     using Counters for Counters.Counter;
 
     // This function get call once when you deploy the contract
     constructor() ERC721("UniversityDegree", "Degree") {
-
+        owner = msg.sender;
     }
 
     // Record which addresses have a degree issued
     mapping(address => bool) public issueDegrees;
 
-    function issueDegree(address to) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;      // '_' is the code of the next function
+    }
+
+    function issueDegree(address to) onlyOwner external {
         issueDegrees[to] = true;
     }
 }
